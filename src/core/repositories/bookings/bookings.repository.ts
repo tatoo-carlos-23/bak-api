1f0e76b6653767c60ddad3911d1b402901052f90
Carlos Abramonte <car.abr.04@gmail.com>
2026-08-10 01:53:53 -0500

    Ajuste en la query sql de verificar disponibilidad de reserva

--- src/core/repositories/bookings/bookings.repository.ts
+++ src/core/repositories/bookings/bookings.repository.ts
@@ -6,8 +6,8 @@ export const checkAvailability = async (
   scheduleId: number,
 ): Promise<ICheckAvailability | null> => {
   const [queryResult] = await mysqlPool.execute(
-    `SELECT boo.id FROM bookings AS boo INNER JOIN schedules as sch ON sch.id = boo.schedule_id INNER JOIN services as ser ON ser.id = sch.service_id WHERE (?  BETWEEN sch.start_date AND sch.end_date) AND (DATE_ADD(?, INTERVAL ser.duration MINUTE) BETWEEN sch.start_date AND sch.end_date) AND boo.schedule_id = ? LIMIT 1;`,
-    [date, date, scheduleId],
+    `SELECT boo.id AS id FROM bookings AS boo INNER JOIN schedules AS sch ON sch.id = boo.schedule_id INNER JOIN services AS ser ON ser.id = sch.service_id WHERE boo.schedule_id = ? AND ? < DATE_ADD(boo.start_time, INTERVAL ser.duration MINUTE) AND DATE_ADD(?, INTERVAL ser.duration MINUTE) > boo.start_time LIMIT 1;`,
+    [scheduleId, date, date],
   );
   return Array.isArray(queryResult)
     ? (queryResult?.[0] as ICheckAvailability)
